a2bd50842c465eb4afc0337cdda14c224814ad36
Adam Workman <adammworkman25@gmail.com>
2020-06-19 20:47:38 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -92,7 +92,7 @@ def all_holidays_with_bbq(holiday_hash)
   
   holiday_hash.each do |season, holiday_supply_hash|
     holiday_supply_hash.each do |holiday, supply|
-      if supply  = "BBQ"
+      if supply.include? "BBQ"
         holidays_with_bbq.push holiday
   end
 end
